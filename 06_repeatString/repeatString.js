f4e3bb6bc8bc5e076cac4817465dd0c9b8872eab
Simon Ischen <74008380+simon-ischen@users.noreply.github.com>
2025-12-19 16:34:57 +0100

    Add finished 06-repeatString task

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
 
+    let joined = "";
+    for (let i = 0; i < num; i++) {
+        joined = joined.concat(string);
+    }
+    return joined;
 };
 
 // Do not edit below this line
